fa50bba2cca6d4a03871f273e6a72e11cc468650
taemon1337 <taemon1337@gmail.com>
2018-07-28 09:42:02 -0400

    added docker

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v $HOME/.local/bin:/usr/local/bin -v $(pwd):/tmp --net host -p 7681:7681 rdkube
+docker run -it --rm -v $HOME/.local/bin:/usr/local/bin -v /var/run/docker.sock:/var/run/docker.sock:ro --net host -p 7681:7681 rdkube
